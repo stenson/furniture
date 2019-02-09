2b3cbfcb8630487e82432c684004a0ef9a383107
Rob Stenson <rob.stenson@gmail.com>
2019-02-09 11:48:10 -0800

    increment to 0.0.8 to avoid conflict with preexisting files uploaded to furniture repo previously

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="furniture",
-    version="0.0.1",
+    version="0.0.8",
     author="Rob Stenson",
     author_email="rob.stenson@gmail.com",
     description="Typesetting and layout utilities for drawing and animating in drawBot",
