49a9ed88fe8efac07e2e9d6c80a2fb5ffe182895
Rob Stenson <rob@goodhertz.com>
2019-06-01 11:06:00 -0700

    use decompose

--- furniture/text.py
+++ furniture/text.py
@@ -1,8 +1,11 @@
 import freetype
+#print(freetype.__file__)
+
 from collections import OrderedDict
 from freetype.raw import *
 from fontParts.fontshell import RGlyph
 from fontTools.pens.transformPen import TransformPen
+from fontTools.pens.recordingPen import RecordingPen, replayRecording
 from fontTools.ttLib import TTFont
 from furniture.geometry import Rect
 import uharfbuzz as hb
@@ -51,6 +54,20 @@ class Harfbuzz():
             x += x_advance + tracking
         return frames
 
+def ft_move_to(a, pen):
+    if len(pen.value) > 0:
+        pen.closePath()
+    pen.moveTo((a.x, a.y))
+
+def ft_line_to(a, pen):
+    pen.lineTo((a.x, a.y))
+
+def ft_conic_to(a, b, pen):
+    pen.qCurveTo((a.x, a.y), (b.x, b.y))
+
+def ft_cubic_to(a, b, c, pen):
+    pen.curveTo((a.x, a.y), (b.x, b.y), (c.x, c.y))
+
 class FreetypeReader():
     def __init__(self, font_path, scale=1000): # should pass in the ttfont not make it
         self.fontPath = font_path
@@ -58,18 +75,23 @@ class FreetypeReader():
         self.font.set_char_size(scale)
         self.scale = scale
         self.ttfont = TTFont(font_path)
-        self.axesOrder = [a.axisTag for a in self.ttfont['fvar'].axes]
+        try:
+            self.axesOrder = [a.axisTag for a in self.ttfont['fvar'].axes]
+        except:
+            self.axesOrder = []
     
     def setVariations(self, axes=dict()):
-        coords = []
-        for name in self.axesOrder:
-            coord = FT_Fixed(axes[name] << 16)
-            coords.append(coord)
-        ft_coords = (FT_Fixed * len(coords))(*coords)
-        flags = freetype.FT_LOAD_DEFAULT | freetype.FT_LOAD_NO_BITMAP
-        freetype.FT_Set_Var_Design_Coordinates(self.font._FT_Face, len(ft_coords), ft_coords)
+        if len(self.axesOrder) > 0:
+            coords = []
+            for name in self.axesOrder:
+                coord = FT_Fixed(axes[name] << 16)
+                coords.append(coord)
+            ft_coords = (FT_Fixed * len(coords))(*coords)
+            flags = freetype.FT_LOAD_DEFAULT | freetype.FT_LOAD_NO_BITMAP
+            freetype.FT_Set_Var_Design_Coordinates(self.font._FT_Face, len(ft_coords), ft_coords)
     
     def setGlyph(self, glyph_id):
+        self.glyph_id = glyph_id
         flags = freetype.FT_LOAD_DEFAULT | freetype.FT_LOAD_NO_BITMAP
         if isinstance(glyph_id, int):
             self.font.load_glyph(glyph_id, flags)
@@ -78,93 +100,17 @@ class FreetypeReader():
 
     def drawOutlineToPen(self, pen, raiseCubics=True):
         outline = self.font.glyph.outline
-        STOP, MOVETO, LINETO, CURVE3, CURVE4 = 0, 1, 2, 3, 4
-        start, end = 0, 0
-        VERTS, CODES = [], []
-        # Iterate over each contour
-        for i in range(len(outline.contours)):
-            end    = outline.contours[i]
-            points = outline.points[start:end+1]
-            points.append(points[0])
-            tags   = outline.tags[start:end+1]
-            tags.append(tags[0])
-
-            segments = [ [points[0],], ]
-            for j in range(1, len(points) ):
-                segments[-1].append(points[j])
-                if ( FT_Curve_Tag( tags[j] ) == FT_Curve_Tag_On ) and j < (len(points)-1):
-                    segments.append( [points[j],] )
-            verts = [points[0], ]
-            codes = [MOVETO,]
-            tags.pop()
-            for segment in segments:
-                if len(segment) == 2:
-                    verts.extend(segment[1:])
-                    codes.extend([LINETO])
-                elif len(segment) == 3:
-                    verts.extend(segment[1:])
-                    codes.extend([CURVE3, CURVE3])
-                elif ( len(segment) == 4 ) \
-                    and ( FT_Curve_Tag(tags[1]) == FT_Curve_Tag_Cubic ) \
-                    and ( FT_Curve_Tag(tags[2]) == FT_Curve_Tag_Cubic ):
-                    verts.extend(segment[1:])
-                    codes.extend([CURVE4, CURVE4, CURVE4])
-                else:
-                    # Interpolating
-                    verts.append(segment[1])
-                    codes.append(CURVE3)
-                    #print(segment)
-                    #print(">>>>>>")
-                    for i in range(1,len(segment)-2):
-                        A,B = segment[i], segment[i+1]
-                        C = ((A[0]+B[0])/2.0, (A[1]+B[1])/2.0)
-                        verts.extend([ C, B ])
-                        codes.extend([ CURVE3, CURVE3])
-                    verts.append(segment[-1])
-                    codes.append(CURVE3)
-                [tags.pop() for x in range(len(segment) - 1)]
-            VERTS.extend(verts)
-            CODES.extend(codes)
-            start = end+1
-        
-        if len(VERTS) == 0: # it's blank
-            return
-        
-        gp = pen
-        i = 0
-        last_point = None
-        while (i < len(CODES)):
-            if (CODES[i] == MOVETO):
-                if last_point:
-                    gp.closePath()
-                gp.moveTo(VERTS[i])
-                last_point = VERTS[i]
-                i += 1
-            elif (CODES[i] == LINETO):
-                gp.lineTo(VERTS[i])
-                last_point = VERTS[i]
-                i += 1
-            elif (CODES[i] == CURVE3):
-                a, b = VERTS[i], VERTS[i+1]
-                # rather than do a qCurve, we infer a cubic
-                if not raiseCubics:
-                    gp.qCurveTo(a, b)
-                    last_point = b
-                else:
-                    #c0 = gp.value[-1][-1][-1]
-                    c0 = last_point
-                    c1 = (c0[0] + (2/3)*(a[0] - c0[0]), c0[1] + (2/3)*(a[1] - c0[1]))
-                    c2 = (b[0] + (2/3)*(a[0] - b[0]), b[1] + (2/3)*(a[1] - b[1]))
-                    c3 = b
-                    gp.curveTo(c1, c2, c3)
-                    last_point = c3
-                i += 2
-            elif (CODES[i] == CURVE4):
-                gp.curveTo(VERTS[i], VERTS[i+1], VERTS[i+2])
-                last_point = VERTS[i+2]
-                i += 3
-        gp.closePath()
-        return gp
+        rp = RecordingPen()
+        self.font.glyph.outline.decompose(rp, move_to=ft_move_to, line_to=ft_line_to, conic_to=ft_conic_to, cubic_to=ft_cubic_to)
+        rp.closePath()
+        replayRecording(rp.value, pen)
+        return
+    
+    def drawTTOutlineToPen(self, pen):
+        glyph_name = self.ttfont.getGlyphName(self.glyph_id)
+        g = self.ttfont.getGlyphSet()[glyph_name]
+        g.draw(pen)
+        print("Glyph name", g)
 
 class StyledString():
     def __init__(self,
@@ -184,11 +130,14 @@ class StyledString():
         
         self.axes = OrderedDict()
         self.variations = dict()
-        for axis in self.ttfont['fvar'].axes:
-            self.axes[axis.axisTag] = axis
-            self.variations[axis.axisTag] = axis.defaultValue
-        for k, v in self.normalizeVariations(variations).items():
-            self.variations[k] = v
+        try:
+            for axis in self.ttfont['fvar'].axes:
+                self.axes[axis.axisTag] = axis
+                self.variations[axis.axisTag] = axis.defaultValue
+            for k, v in self.normalizeVariations(variations).items():
+                self.variations[k] = v
+        except:
+            pass
     
     def normalizeVariations(self, variations):
         for k, v in variations.items():
@@ -242,7 +191,7 @@ class StyledString():
     def formattedString(self):
         feas = dict(self.features)
         del feas["kern"]
-        return FormattedString(self.text, font=self.fontFile, fontSize=self.fontSize, lineHeight=self.fontSize+1, tracking=self.tracking, fontVariations=self.variations, openTypeFeatures=feas)
+        return FormattedString(self.text, font=self.fontFile, fontSize=self.fontSize, lineHeight=self.fontSize+2, tracking=self.tracking, fontVariations=self.variations, openTypeFeatures=feas)
     
     def drawToPen(self, out_pen):
         fr = FreetypeReader(self.fontFile, scale=1000)
@@ -252,15 +201,22 @@ class StyledString():
             fr.setGlyph(frame.gid)
             fill(0, 0.5, 1, 0.5)
             s = self.fontSize/1000
-            tp_scale = TransformPen(out_pen, (s, 0, 0, s, 0, 0))
-            tp_transform = TransformPen(tp_scale, (1, 0, 0, 1, frame.frame.x, 0))
-            fr.drawOutlineToPen(tp_transform, raiseCubics=True)
+            if False:
+                tp_scale = TransformPen(out_pen, (s, 0, 0, s, 0, 0))
+                tp_transform = TransformPen(tp_scale, (1, 0, 0, 1, frame.frame.x, frame.frame.y))
+                fr.drawOutlineToPen(tp_transform, raiseCubics=True)
+            else:
+                fr.drawOutlineToPen(out_pen, raiseCubics=True)
+                bp = BezierPath()
+                fr.drawTTOutlineToPen(bp)
+                drawPath(bp)
+                #drawBezierSkeleton(bp)
 
 if __name__ == "__main__":
     import os
     import time
     from defcon import Glyph
-    from grafutils.beziers.utils import drawBezier
+    from grafutils.beziers.utils import drawBezier, drawBezierSkeleton
     
     fp = os.path.expanduser("~/Library/Fonts")
     faces = [
@@ -287,22 +243,23 @@ if __name__ == "__main__":
             print("DIFF: w={:.2f}, t={:.2f}".format(w1 - w2, l2 / l1))
             print("----------")
     
-    def test_styled_string():
+    def test_styled_fitting():
         fill(0)
         rect(*Rect.page())
         
         ss = StyledString("COMPRESSION".upper(),
             fontFile=f"{fp}/ObviouslyVariable.ttf",
             fontSize=55,
-            variations=dict(wdth=1.0, wght=0.0),
-            features=dict(ss01=True), # not working
-            tracking=200)
-        count = 30
+            variations=dict(wdth=1.0, wght=0.2),
+            features=dict(ss01=True),
+            tracking=30)
+        count = 22
+        translate(0, 6)
         for x in range(count):
-            w = (pow(1-x/count, 2))*1000
+            w = 100+(pow(1-x/count, 2))*900
             if False:
                 print("BEFORE wdth", ss.variations.get("wdth"),
-                "width", ss.width(), ss.tracking)
+                    "width", ss.width(), ss.tracking)
             ss.fit(w, variationLimits=dict(wdth="min"))
             if False:
                 print("AFTER wdth", ss.variations.get("wdth"),
@@ -319,4 +276,45 @@ if __name__ == "__main__":
                 bp.translate(4, -74)
                 drawPath(bp)
     
-    test_styled_string()
+    def test_styled_string(t, f):
+        size(2000, 900)
+        #translate(200, 200)
+        #with savedState():
+        #    image("~/Desktop/hb.png", (0, 0))
+        translate(35, 124)
+        ss = StyledString(t.upper(),
+            fontFile=f,
+            fontSize=1000,
+            tracking=0)
+        g = Glyph()
+        ss.drawToPen(g.getPen())
+        fill(0, 0.5, 1, 0.5)
+        #translate(34, 125)
+        #drawBezier(g)
+        drawBezierSkeleton(g, labels=True)
+        if False: # also draw a coretext string?
+            fill(None)
+            stroke(1, 0, 0.5, 0.25)
+            strokeWidth(2)
+            bp = BezierPath()
+            bp.text(ss.formattedString(), (0, 0))
+            bp.removeOverlap()
+            bp.translate(0, 10)
+            #bp.translate(4, -74)
+            #drawBezierSkeleton(bp, labels=True)
+            drawPath(bp)
+    
+    #test_styled_fitting()
+    
+    t = "ٱلْـحَـمْـدُ للهِ‎"
+    t = "نستعلیق"
+    #t = "ن"
+    #t = "﮲"
+    f = f"{fp}/AwamiNastaliq-Regular.ttf"
+    f = f"{fp}/29LTArapix.otf"
+    f = "/Users/robertstenson/Library/Application Support/.FCache/.R-5-17198-18-jaivlwiaijqk-jtiks.otf"
+    f = "/Users/robertstenson/Type/fonts/fonts/29LTAzal-Display.ttf"
+    
+    t = "I"
+    f = "/Users/robertstenson/Library/Fonts/Beastly-72Point.otf"
+    test_styled_string(t, f)
